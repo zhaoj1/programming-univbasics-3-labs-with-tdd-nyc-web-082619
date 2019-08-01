b170dc7b0eb6ee9f93305f69a7f5b6b3fae4073f
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-01 14:49:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,7 @@
 # Add your variable
 def first_number = 1
 def second_number = 2
+def sum = first_number + second_number
+def difference = first_number - second_number
+def product = first_number * second_number
+def quotient = first_number / second_number
